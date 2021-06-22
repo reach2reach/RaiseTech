450f8a96102f02ae05f169788949009901d0b151
reach2reach <reachbook@rie-no-MacBook.local>
2021-06-22 23:59:03 +0900

    second commit

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,11 +1,17 @@
 //ハンバーガーメニュー
-  $(function() {
-    $(".is-hamburger").on("click",function(){ //clickしたらis-openが加わる
-      $(this).toggleClass('is-open');
-      $(".global-nav").slideToggle('is-open');   //global-navにis-openのクラス
-      $("body") .toggleClass("is-open");
-    });
-   });
+
+$(function() {
+  $(".is-hamburger").on("click",function(){ //clickしたらis-openが加わる
+    $(this).toggleClass('is-open');
+    $(".global-nav").toggleClass('is-open');   //global-navにis-openのクラス
+    $("body").toggleClass("is-open");
+  });
+ });
+
+
+  
+
+  
 
 
 
